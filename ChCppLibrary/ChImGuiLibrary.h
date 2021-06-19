0951b015f1fe096809be0aa0cf2d180f751e5bea
Chronoss0518 <sinyusuke5000@gmail.com>
2021-06-19 18:11:51 +0900

    一次更新完了

--- ChCppLibrary/ChImGuiLibrary.h
+++ ChCppLibrary/ChImGuiLibrary.h
@@ -1,34 +1,8 @@
 #ifndef Ch_ImGui_BAPI_h
 #define Ch_ImGui_BAPI_h
 
-#ifndef IMGUI_VERSION
-
-#include"External/imgui/imconfig.h"
-#include"External/imgui/imgui.h"
-
-#endif
-
 #include"BaseIncluder/ChBase.h"
-#include"CPP/ChBitBool/ChBitBool.h"
 
 #include"ImGUI/ChImGUIBaseWind/ChImGUIBaseWind.h"
-
-#ifdef _WINDOWS_
-
-#include"External/imgui/backends/imgui_impl_win32.h"
-
-#ifdef _D3D9_H_
-
-#include"External/imgui/backends/imgui_impl_dx9.h"
-
-#endif//_D3D9_H_
-
-#ifdef __d3d11_h__
-
-#include"External/imgui/backends/imgui_impl_dx11.h"
-
-#endif//__d3d11_h__
-
-#endif//_WINDOWS_
 
 #endif
