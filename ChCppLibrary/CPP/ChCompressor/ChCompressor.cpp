553ca194ad5c089bc9bc7172293e26e0c1579761
Chronoss0518 <sinyusuke5000@gmail.com>
2021-07-10 15:23:47 +0900

    Compressorの機能を追加

--- ChCppLibrary/CPP/ChCompressor/ChCompressor.cpp
+++ ChCppLibrary/CPP/ChCompressor/ChCompressor.cpp
@@ -76,7 +76,35 @@ std::vector<char> Rungless::Press(const std::vector<char>& _pressBase)
 	char targetChar = 0;
 	unsigned long targetCharCount = 0;
 
+	unsigned long maxSize = 0;
+
+	for (unsigned long i = 0; i < _pressBase.size(); i++)
+	{
+		if (i <= 0)
+		{
+			targetChar = _pressBase[i];
+		}
+		else if (targetChar != _pressBase[i])
+		{
+			maxSize = targetCharCount > maxSize ? targetCharCount : maxSize;
+
+			targetChar = _pressBase[i];
+			targetCharCount = 0;
+		}
+		targetCharCount++;
+	}
+
+	char biteSize = maxSize < 0xff ? 1 : maxSize < 0xffff ? 2 : maxSize < 0xffffffff ? 4 : 8;
+
 	std::vector<char> out;
+
+	out.push_back(biteSize);
+	out.push_back('\0');
+
+
+	targetChar = 0;
+	targetCharCount = 0;
+
 
 	for (unsigned long i = 0; i < _pressBase.size(); i++)
 	{
@@ -90,24 +118,24 @@ std::vector<char> Rungless::Press(const std::vector<char>& _pressBase)
 
 			void* tmp = &targetCharCount;
 
-			for (unsigned char j = 0; j < 4; j++)
+			for (unsigned char j = 0; j < biteSize; j++)
 			{
-				out.push_back(static_cast<char*>(tmp)[j]);
+				out.push_back(static_cast<char*>(tmp)[biteSize - j - 1]);
 			}
 
 			targetChar = _pressBase[i];
 			targetCharCount = 0;
 		}
+		targetCharCount++;
 	}
-
 
 	out.push_back(targetChar);
 
 	void* tmp = &targetCharCount;
 
-	for (unsigned char j = 0; j < 4; j++)
+	for (unsigned char j = 0; j < biteSize; j++)
 	{
-		out.push_back(static_cast<char*>(tmp)[j]);
+		out.push_back(static_cast<char*>(tmp)[biteSize - j - 1]);
 	}
 
 	return out;
--- ChCppLibrary/CPP/ChCompressor/ChRunglessCompressor.h
+++ ChCppLibrary/CPP/ChCompressor/ChRunglessCompressor.h
@@ -17,6 +17,8 @@ namespace ChCpp
 
 		private:
 
+
+
 		};
 
 	}
