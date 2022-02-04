9ed752b182a5990fea803189dcedf777767ca600
Chronoss0518 <sinyusuke5000@gmail.com>
2022-02-05 00:46:29 +0900

    PolygonBoardの動作テストが完了

--- ChCppLibrary/Direct3D11/ChPolygonBoard/ChPolygonBoard11.h
+++ ChCppLibrary/Direct3D11/ChPolygonBoard/ChPolygonBoard11.h
@@ -35,7 +35,7 @@ namespace ChD3D11
 			{
 				if (_num >= 4)return ChVec3();
 
-				return primitives.vertexArray[_num].pos;
+				return vertexs[_num].pos;
 
 			}
 
@@ -43,7 +43,7 @@ namespace ChD3D11
 			{
 				if (_num >= 4)return ChVec2();
 
-				return primitives.vertexArray[_num].uvPos;
+				return vertexs[_num].uvPos;
 
 			}
 
@@ -56,11 +56,10 @@ namespace ChD3D11
 
 		protected:
 
-			void UpdateVertex();
 
-			///////////////////////////////////////////////////////////////////////////////////
-
-			ChStd::Bool updateFlg = true;
+			MeshVertex11 vertexs[4];
+			IndexBuffer11 indexBuf;
+			VertexBuffer11<MeshVertex11>vertexBuf;
 
 			PrimitiveData11<MeshVertex11> primitives;
 	};
