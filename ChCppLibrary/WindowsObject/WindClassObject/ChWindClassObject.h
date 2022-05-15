ec568c55f6a3e118a7dcbf0a4d756dcc2cba8c5c
Chronoss0518 <sinyusuke5000@gmail.com>
2022-05-15 18:08:31 +0900

    WindClassObjectでwchar_tとchar型に対応

--- ChCppLibrary/WindowsObject/WindClassObject/ChWindClassObject.h
+++ ChCppLibrary/WindowsObject/WindClassObject/ChWindClassObject.h
@@ -8,21 +8,21 @@ namespace ChWin
 	class WindClassStyle;
 
 	//WindowsAPIの内WindowClassを用いたオブジェクトを司るクラス//
-	class WindClassObject:public ChCp::Initializer
+	class WindClassObjectA:public ChCp::Initializer
 	{
 	public://ConstructorDestructor//
 
-		inline WindClassObject() { Init(); }
+		inline WindClassObjectA() { Init(); }
 
-		inline WindClassObject(const WNDCLASS& _cls) { Init(_cls); }
+		inline WindClassObjectA(const WNDCLASSA& _cls) { Init(_cls); }
 
-		inline ~WindClassObject() { Release(); }
+		inline ~WindClassObjectA() { Release(); }
 		
 	public://InitAndRelease//
 
 		void Init();
 
-		void Init(const WNDCLASS& _cls);
+		void Init(const WNDCLASSA& _cls);
 
 		void Release();
 
@@ -48,22 +48,101 @@ namespace ChWin
 
 		void SetWindProcedure(WNDPROC _windProc);
 
+		void SetMenuName(const std::string& _name);
+
 	public://GetFunction//
 
 		inline std::string GetWindClassName() { return className; }
 
 	public:
 
+		ChStd::Bool IsSystemRegistClassName(const std::string& _className);
+
+	public://Other Fucntions//
+
 		//Set関数よりセットされた情報を元にクラスを登録する//
 		//クラス名が空文字の場合は登録されない//
 		void RegistClass(const std::string& _className);
 
+
 	protected:
 
-		WNDCLASS cls;
+		WNDCLASSA cls;
 		std::string className = "";
-
 	};
+
+	//WindowsAPIの内WindowClassを用いたオブジェクトを司るクラス//
+	class WindClassObjectW :public ChCp::Initializer
+	{
+	public://ConstructorDestructor//
+
+		inline WindClassObjectW() { Init(); }
+
+		inline WindClassObjectW(const WNDCLASSW& _cls) { Init(_cls); }
+
+		inline ~WindClassObjectW() { Release(); }
+
+	public://InitAndRelease//
+
+		void Init();
+
+		void Init(const WNDCLASSW& _cls);
+
+		void Release();
+
+	public://SetFunction//
+
+		void SetStyle(const unsigned int _style);
+
+		void SetStyle(const WindClassStyle* _style);
+
+		void SetIcon(const HICON _icon);
+
+		void SetCursol(const HCURSOR _cursor);
+
+		//ウィンドウクラス構造の後の追加バイト数//
+		void SetClsExtra(const int _byteNum = 0);
+
+		//ウィンドウインスタンスの後の追加バイト数//
+		void SetWndExtra(const int _byteNum = 0);
+
+		void SetBackgroundColor(const HBRUSH _brush);
+
+		void SetInstance(const HINSTANCE _hInst);
+
+		void SetWindProcedure(WNDPROC _windProc);
+
+		void SetMenuName(const std::wstring& _name);
+
+	public://GetFunction//
+
+		inline std::wstring GetWindClassName() { return className; }
+
+	public:
+
+		ChStd::Bool IsSystemRegistClassName(const std::wstring& _className);
+
+	public://Other Fucntions//
+
+		//Set関数よりセットされた情報を元にクラスを登録する//
+		//クラス名が空文字の場合は登録されない//
+		void RegistClass(const std::wstring& _className);
+
+
+	protected:
+
+		WNDCLASSW cls;
+		std::wstring className = L"";
+};
+
+
+	using WindClassObject =
+#ifdef UNICODE
+		WindClassObjectW;
+#else
+		WindClassObjectA;
+#endif
+
 }
 
 #endif
