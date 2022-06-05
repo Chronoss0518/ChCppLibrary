dddb8dc51475d22c546aec6065b3a9903394fcf1
Chronoss0518 <sinyusuke5000@gmail.com>
2022-06-05 15:48:49 +0900

    MsgBoxを大型変更

--- ChCppLibrary/WindowsObject/MsgBox/ChMsgBox.cpp
+++ ChCppLibrary/WindowsObject/MsgBox/ChMsgBox.cpp
@@ -1,6 +1,72 @@
 #include<Windows.h>
 #include"../../BaseIncluder/ChBase.h"
 
+#include"../PackData/ChPoint.h"
 #include"ChMsgBox.h"
 
 using namespace ChWin;
+
+
+MsgBox::PushButtonType MsgBox::DisplayW(
+	HWND _hWnd,
+	const wchar_t* _text,
+	const wchar_t* _title)
+{
+
+	if (buttonType <= 0)return PushButtonType::Error;
+	unsigned int uType = CreateUType();
+	uType |= MB_SERVICE_NOTIFICATION;
+
+	return static_cast<PushButtonType>(MessageBoxW(_hWnd, _text, _title, uType));
+}
+
+MsgBox::PushButtonType MsgBox::DisplayServiceNotificationW(
+	const wchar_t* _text,
+	const wchar_t* _title)
+{
+
+	if (buttonType <= 0)return PushButtonType::Error;
+	unsigned int uType = CreateUType();
+	uType |= MB_SERVICE_NOTIFICATION;
+
+	return static_cast<PushButtonType>(MessageBoxW(nullptr, _text, _title, uType));
+}
+
+
+MsgBox::PushButtonType MsgBox::DisplayA(
+	HWND _hWnd,
+	const char* _text,
+	const char* _title)
+{
+
+	if (buttonType <= 0)return PushButtonType::Error;
+
+	unsigned int uType = CreateUType();
+	uType |= MB_SERVICE_NOTIFICATION;
+
+	return static_cast<PushButtonType>(MessageBoxA(_hWnd, _text, _title, uType));
+}
+
+MsgBox::PushButtonType MsgBox::DisplayServiceNotificationA(
+	const char* _text,
+	const char* _title)
+{
+	if (buttonType <= 0)return PushButtonType::Error;
+
+	unsigned int uType = CreateUType();
+	uType |= MB_SERVICE_NOTIFICATION;
+
+	return static_cast<PushButtonType>(MessageBoxA(nullptr, _text, _title, uType));
+}
+
+
+unsigned int MsgBox::CreateUType()
+{
+	unsigned int uType = buttonType;
+	uType |= ChStd::EnumCast(mType);
+	uType |= ChStd::EnumCast(diType);
+	uType |= ChStd::EnumCast(tType);
+	uType |= ChStd::EnumCast(type);
+	return uType;
+
+}
--- ChCppLibrary/WindowsObject/MsgBox/ChMsgBox.h
+++ ChCppLibrary/WindowsObject/MsgBox/ChMsgBox.h
@@ -3,15 +3,21 @@
 #ifndef Ch_Win_BMsg_h
 #define Ch_Win_BMsg_h
 
+#ifndef Ch_Win_WO_h
+#include"../WindObject/ChWindObject.h"
+#endif
+
 namespace ChWin
 {
+
 	//WindowsAPIのメッセージボックスを容易に表示させるクラス//
 	class MsgBox
 	{
-	public:
-		
-		enum class PushButtonType : int
+	public://Inner Enum Classes//
+
+		typedef enum class PushButtonType : int
 		{
+			Error = 0,
 			Ok = IDOK,
 			Cancel = IDCANCEL,
 			Abort = IDABORT,
@@ -21,9 +27,9 @@ namespace ChWin
 			No = IDNO,
 			Tryagain = IDTRYAGAIN,
 			Continue = IDCONTINUE
-		};
+		}PBType;
 
-		enum DisplayButtonType :unsigned int
+		typedef enum class DisplayButtonType :unsigned int
 		{
 			Ok = MB_OK,
 			OkCancel = MB_OKCANCEL,
@@ -33,111 +39,149 @@ namespace ChWin
 			RetryCancel = MB_RETRYCANCEL,
 			CancelRetryContinue = MB_CANCELTRYCONTINUE,
 			Help = MB_HELP,
-		};
 
-		enum class DefaultButtonType : unsigned int
+		}DispBType;
+
+		typedef enum class DefaultButtonType : unsigned int
 		{
 			Button1 = MB_DEFBUTTON1,
 			Button2 = MB_DEFBUTTON2,
 			Button3 = MB_DEFBUTTON3,
 			Button4 = MB_DEFBUTTON4
-		};
+		}DefBType;
 
-		enum class ModalType : unsigned int
+		typedef enum class ModalType : unsigned int
 		{
 			None = 0x0L,
 			Application = MB_APPLMODAL,
 			System = MB_SYSTEMMODAL,
 			Task = MB_TASKMODAL
-		};
+		}MType;
 
-		enum DisplayIconType : unsigned int
+		typedef enum class DisplayIconType : unsigned int
 		{
+			None = 0x0L,
 			Stop = MB_ICONSTOP,
 			Question = MB_ICONQUESTION,
 			Warning = MB_ICONWARNING,
 			Information = MB_ICONINFORMATION
-		};
+		}DIType;
 
-		enum TextType : unsigned int
+		typedef enum class TextType : unsigned int
 		{
+			None = 0x0L,
 			Right = MB_RIGHT,
 			RTLReading = MB_RTLREADING
-		};
+		}TType;
 
-		enum MsgBoxType :unsigned int
+		typedef enum class MsgBoxType :unsigned int
 		{
+			None = 0x0L,
 			SetForegRound = MB_SETFOREGROUND,
 			TopMost = MB_TOPMOST,
-		};
+		}MBType;
 
+	public://Set Functions//
+
+		inline void SetDefaultButtonType(const DefaultButtonType _type)
+		{
+			buttonType = ChStd::EnumCast(_type);
+		}
+
+		inline void SetModalType(const ModalType _type)
+		{
+			mType = _type;
+		}
+
+		inline void SetDisplayIconType(const DisplayIconType _type)
+		{
+			diType = _type;
+		}
+
+		inline void SetTextType(const TextType _type)
+		{
+			tType = _type;
+		}
+
+		inline void SetMsgBoxType(const MsgBoxType _type)
+		{
+			type = _type;
+		}
+
+	public://Other Functions//
+
+		inline void AddDisplayButtonType(const DisplayButtonType _type)
+		{
+			buttonType |= ChStd::EnumCast(_type);
+		}
+
+		inline void ClearDisplayButtonType(const DisplayButtonType _type)
+		{
+			buttonType = 0;
+		}
+
+		PushButtonType DisplayW(
+			HWND _hWnd,
+			const wchar_t* _text,
+			const wchar_t* _title);
+
+		PushButtonType DisplayServiceNotificationW(
+			const wchar_t* _text,
+			const wchar_t* _title);
+
+		PushButtonType DisplayA(
+			HWND _hWnd,
+			const char* _text,
+			const char* _title);
+
+		PushButtonType DisplayServiceNotificationA(
+			const char* _text,
+			const char* _title);
+
+		PushButtonType Display(
+			HWND _hWnd,
 #ifdef _UNICODE
-
-		inline static PushButtonType Display(
-			HWND _hWnd,
 			const wchar_t* _text,
-			const wchar_t* _title,
-			const unsigned int _uType,
-			const DefaultButtonType _dType = DefaultButtonType::Button1,
-			const ModalType _mType = ModalType::None)
-		{
-
-			unsigned int uType = _uType;
-			uType |= ChStd::EnumCast(_dType);
-			uType |= ChStd::EnumCast(_mType);
-
-			return (PushButtonType)MessageBoxW(_hWnd, _text, _title, uType);
-		}
-
-		inline static PushButtonType DisplayServiceNotification(
-			const wchar_t* _text,
-			const wchar_t* _title,
-			const unsigned int _uType,
-			const DefaultButtonType _dType = DefaultButtonType::Button1,
-			const ModalType _mType = ModalType::None)
-		{
-
-			unsigned int uType = _uType;
-			uType |= ChStd::EnumCast(_dType);
-			uType |= ChStd::EnumCast(_mType);
-			uType |= MB_SERVICE_NOTIFICATION;
-
-			return (PushButtonType)MessageBoxW(nullptr, _text, _title, uType);
-		}
-
+			const wchar_t* _title
+#else
+			const char* _text,
+			const char* _title
 #endif
-
-		inline static PushButtonType Display(
-			HWND _hWnd,
-			const char* _text,
-			const char* _title,
-			const unsigned int _uType,
-			const DefaultButtonType _dType = DefaultButtonType::Button1,
-			const ModalType _mType = ModalType::None)
+		)
 		{
-
-			unsigned int uType = _uType;
-			uType |= ChStd::EnumCast(_dType);
-			uType |= ChStd::EnumCast(_mType);
-
-			return (PushButtonType)MessageBoxA(_hWnd, _text, _title, uType);
+#ifdef _UNICODE
+			return DisplayW(_hWnd, _text, _title);
+#else
+			return DisplayA(_hWnd, _text, _title);
+#endif
 		}
 
-		inline static PushButtonType DisplayServiceNotification(
+		PushButtonType DisplayServiceNotification(
+#ifdef _UNICODE
+			const wchar_t* _text,
+			const wchar_t* _title
+#else
 			const char* _text,
-			const char* _title,
-			const unsigned int _uType,
-			const DefaultButtonType _dType = DefaultButtonType::Button1,
-			const ModalType _mType = ModalType::None)
+			const char* _title
+#endif
+		)
 		{
-			unsigned int uType = _uType;
-			uType |= ChStd::EnumCast(_dType);
-			uType |= ChStd::EnumCast(_mType);
-			uType |= MB_SERVICE_NOTIFICATION;
-
-			return (PushButtonType)MessageBoxA(nullptr, _text, _title, uType);
+#ifdef _UNICODE
+			return DisplayServiceNotificationW(_text, _title);
+#else
+			return DisplayServiceNotificationA(_text, _title);
+#endif
 		}
 
+		private:
+
+			unsigned int CreateUType();
+
+			unsigned int buttonType = 0;
+			ModalType mType = ModalType::None;
+			DisplayIconType diType = DisplayIconType::None;
+			TextType tType = TextType::None;
+			MsgBoxType type = MsgBoxType::None;
 	};
 }
 
