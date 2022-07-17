7e8919182421fe23f41c7953ffa83744b098f37d
Chronoss0518 <sinyusuke5000@gmail.com>
2022-07-17 19:38:40 +0900

    Libraryで用意したConstantBufferを管理する基本クラスを作成

---
+++ ChCppLibrary/Direct3D11/ChCB/ChCBBase11.cpp
@@ -0,0 +1,33 @@
+#include<Windows.h>
+#include"../../BaseIncluder/ChBase.h"
+#include"../../BaseIncluder/ChD3D11I.h"
+
+#include"../ChTexture/ChTexture11.h"
+#include"ChCBBase11.h"
+
+using namespace ChD3D11;
+using namespace CB;
+
+void CBBase11::Init(ID3D11Device* _device)
+{
+	device = _device;
+
+	SetInitFlg(true);
+}
+
+void CBBase11::SetShaderTexture(ID3D11DeviceContext* _dc, ChPtr::Weak<TextureBase11> _tex, TextureBase11& _defaultTex, const unsigned long _registerNo)
+{
+
+	TextureBase11* tmpTex = &_defaultTex;
+
+	if (!_tex.expired())
+	{
+		auto tex = _tex.lock();
+		if (tex->IsTex())
+		{
+			tmpTex = tex.get();
+		}
+	}
+
+	tmpTex->SetDrawData(_dc, _registerNo);
+}
