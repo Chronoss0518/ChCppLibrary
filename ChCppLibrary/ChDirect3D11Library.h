1a407f883bfe1cdf20f2380b1a748422484e66fc
Chronoss0518 <sinyusuke5000@gmail.com>
2022-08-15 16:42:46 +0900

    他のプロジェクトで容易に利用できるように変更

--- ChCppLibrary/ChDirect3D11Library.h
+++ ChCppLibrary/ChDirect3D11Library.h
@@ -24,7 +24,7 @@
 #include"Direct3D11/ChCB/ChCBSprite/ChCBSprite11.h"
 #include"Direct3D11/ChCB/ChCBLight/ChCBLight11.h"
 //サンプルシェーダー//
-
+#include"Direct3D11/ChSampleShader/PolygonShader/ChBaseDrawMesh11.h"
 
 //空間操作//
 #include"Direct3D11/ChCameraController/ChCameraController11.h"
