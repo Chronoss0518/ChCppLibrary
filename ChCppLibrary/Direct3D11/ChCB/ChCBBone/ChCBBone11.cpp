32efc88fb343d99a5808e69fbb40543bbcd89f47
Chronoss <sinyusuke5000@gmail.com>
2023-01-22 17:37:42 +0900

    スキンメッシュアニメーションで利用するボーンの情報を保持したクラスを作成

---
+++ ChCppLibrary/Direct3D11/ChCB/ChCBBone/ChCBBone11.cpp
@@ -0,0 +1,97 @@
+#include<Windows.h>
+#include"../../../BaseIncluder/ChBase.h"
+#include"../../../BaseIncluder/ChD3D11I.h"
+
+#include"../../ChTexture/ChTexture11.h"
+#include"ChCBBone11.h"
+
+using namespace ChD3D11;
+using namespace CB;
+
+///////////////////////////////////////////////////////////////////////////////////
+//LightHeader Method
+///////////////////////////////////////////////////////////////////////////////////
+
+void CBBone11::Init(ID3D11Device* _device)
+{
+	Release();
+
+	CBBase11::Init(_device);
+
+	buf.CreateBuffer(_device, BONE_DATA_REGISTERNO);
+
+	SetInitFlg(true);
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+
+void CBBone11::Release()
+{
+	if (!*this)return;
+
+	buf.Release();
+
+	SetInitFlg(false);
+}
+
+void CBBone11::SetBoneObjectDrawMatrix(const ChLMat& _mat, const unsigned long _no)
+{
+	if (!*this)return;
+	if (_no >= BONE_MAX_NUM)return;
+
+	bone.skinWeightMat[_no] = _mat;
+
+	updateFlg = true;
+}
+
+void CBBone11::SetBoneOffsetMatrix(const ChLMat& _mat, const unsigned long _no)
+{
+	if (!*this)return;
+	if (_no >= BONE_MAX_NUM)return;
+
+	bone.boneOffsetMat[_no] = _mat;
+	bone.boneOffsetInverseMat[_no] = _mat;
+	bone.boneOffsetInverseMat[_no].Inverse();
+	updateFlg = true;
+}
+
+void CBBone11::SetPSDrawData(ID3D11DeviceContext* _dc)
+{
+	if (!*this)return;
+
+	Update(_dc);
+
+	buf.SetToPixelShader(_dc);
+
+}
+
+void CBBone11::SetVSDrawData(ID3D11DeviceContext* _dc)
+{
+	if (!*this)return;
+
+	Update(_dc);
+
+	buf.SetToVertexShader(_dc);
+
+}
+
+void CBBone11::SetDrawData(ID3D11DeviceContext* _dc)
+{
+
+	if (!*this)return;
+
+	Update(_dc);
+
+	buf.SetToVertexShader(_dc);
+	buf.SetToPixelShader(_dc);
+
+}
+
+void CBBone11::Update(ID3D11DeviceContext* _dc)
+{
+	if (!updateFlg)return;
+
+	buf.UpdateResouce(_dc, &bone);
+
+	updateFlg = false;
+}
