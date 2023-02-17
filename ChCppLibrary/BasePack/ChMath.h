3669d25977d50f454cf47d893afa91fa0464368c
Chronoss <sinyusuke5000@gmail.com>
2023-02-17 23:22:39 +0900

    機能部分をCppへ移動

--- ChCppLibrary/BasePack/ChMath.h
+++ ChCppLibrary/BasePack/ChMath.h
@@ -4,87 +4,15 @@
 namespace ChMath
 {
 
-	static inline std::string ConvertNum10to64(unsigned long _num)
-	{
-		return "";
-	}
+	float Round(const float& _val, const unsigned int _digit);
 
-	static inline float Round(const float& _val, const unsigned int _digit)
-	{
-		float out = _val * std::powf(10.0f, static_cast<float>(_digit - 1));
-		out = std::round(out);
-		out = out * std::powf(0.1f, static_cast<float>(_digit - 1));
+	double Round(const double& _val, const unsigned int _digit);
 
-		return out;
-	}
+	long double SqrtEx(const long double& _base, const unsigned long _digit = 4931);
 
-	///////////////////////////////////////////////////////////////////////////////////
+	double SqrtEx(const double& _base, const unsigned long _digit = 307);
 
-	static inline double Round(const double& _val, const unsigned int _digit)
-	{
-		double out = _val * std::powl(10, static_cast<double>(_digit - 1));
-		out = std::round(out);
-		out = out * std::powl(0.1, static_cast<double>(_digit - 1));
-
-
-		return out;
-
-	}
-
-	static inline long double SqrtEx(const long double& _base, const unsigned long _digit = 4931)
-	{
-		if (_base == 0.0)return 0.0;
-
-		long double out = std::sqrt(_base);
-
-		//微分積分自分で使えるようになってから再度考える//
-		return out;
-
-		unsigned long maxCount = _digit > 4931 ? 4931 : _digit;
-
-		//以下を参照//
-		//https://qiita.com/rytaryu/items/e5d760a80f9ce5db860f
-		//
-
-		for (unsigned long i = 0; i < maxCount; i++)
-		{
-			out = ((out * out) + _base) / (2 * out);
-		}
-
-		return out;
-	}
-
-	///////////////////////////////////////////////////////////////////////////////////
-
-	static inline double SqrtEx(const double& _base, const unsigned long _digit = 307)
-	{
-		if (_base == 0.0)return 0.0;
-
-		double out = _base;
-
-		unsigned long maxCount = _digit > 307 ? 307 : _digit;
-
-		out = static_cast<double>(SqrtEx(static_cast<long double>(out), maxCount));
-
-		return out;
-	}
-
-	///////////////////////////////////////////////////////////////////////////////////
-
-	static inline float SqrtEx(const float& _base, const unsigned long _digit = 37)
-	{
-		if (_base == 0.0f)return 0.0f;
-
-		float out = _base;
-
-		unsigned long maxCount = _digit > 37 ? 37 : _digit;
-
-		out = static_cast<float>(SqrtEx(static_cast<long double>(out), maxCount));
-
-		return out;
-	}
-
-	///////////////////////////////////////////////////////////////////////////////////
+	float SqrtEx(const float& _base, const unsigned long _digit = 37);
 
 	//2の平方根(有効桁数8桁)//
 	static const float SQUARE_ROOT = 1.41421356f;
