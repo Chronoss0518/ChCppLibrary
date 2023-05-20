df9e8b306b928b560f9790ed71fe50c4af545007
Chronoss <sinyusuke5000@gmail.com>
2023-05-20 18:06:19 +0900

    JsonBaseClassを作成

--- ChCppLibrary/CPP/ChJsonObject/ChJsonObject.cpp
+++ ChCppLibrary/CPP/ChJsonObject/ChJsonObject.cpp
@@ -1,5 +1,90 @@
-
 #include"../../BaseIncluder/ChBase.h"
+#include<cmath>
 
-#include"../ChBaseObject/ChBaseObject.h"
 #include"ChJsonObject.h"
+#include"../ChCumulative/ChCumulative.h"
+
+using namespace ChCpp;
+
+bool JsonObjectBase::IsObject(const std::string& _json)
+{
+
+	Cumulative<char> cumulativeObjectChecker = Cumulative<char>('{', '}');
+	Cumulative<char> cumulativeArrayChecker = Cumulative<char>('[', ']');
+	unsigned long stringDQuotation = 0;
+	unsigned long stringSQuotation = 0;
+
+	cumulativeObjectChecker.Update(_json[0]);
+
+	if (cumulativeObjectChecker.GetCount() <= 0)return false;
+
+	for (unsigned long i = 1; i < _json.length(); i++)
+	{
+		cumulativeObjectChecker.Update(_json[i]);
+		cumulativeArrayChecker.Update(_json[i]);
+
+		if (_json[i] == '\'' && stringDQuotation % 2 <= 0)stringSQuotation += 1;
+		if (_json[i] == '\"' && stringSQuotation % 2 <= 0)stringDQuotation += 1;
+
+	}
+
+	if (cumulativeObjectChecker.GetCount() > 0)return false;
+	if (cumulativeArrayChecker.GetCount() > 0)return false;
+	if (stringDQuotation % 2 > 0)return false;
+	if (stringSQuotation % 2 > 0)return false;
+
+	return true;
+}
+
+bool JsonObjectBase::IsArray(const std::string& _json)
+{
+
+}
+
+bool JsonObjectBase::IsString(const std::string& _json)
+{
+
+}
+
+bool JsonObjectBase::IsNumber(const std::string& _json)
+{
+
+}
+
+bool JsonObjectBase::IsBoolean(const std::string& _json)
+{
+
+}
+
+bool JsonObject::SetRawData(const std::string& _jsonText)
+{
+	Cumulative<char> cumulativeObjectChecker = Cumulative<char>('{', '}');
+
+	cumulativeObjectChecker.Update(_jsonText[0]);
+
+	if (cumulativeObjectChecker.GetCount() <= 0)return false;
+
+
+
+
+	return true;
+
+}
+
+std::string JsonObject::GetRawData()const
+{
+	std::string res = "{";
+
+	bool initFlg = false;
+
+	for (auto&& val : value)
+	{
+		if (initFlg)res += ",";
+		res += val.first + ":" + val.second->GetRawData();
+		initFlg = true;
+	}
+
+	res += "}";
+
+	return res;
+}
--- ChCppLibrary/CPP/ChJsonObject/ChJsonObject.h
+++ ChCppLibrary/CPP/ChJsonObject/ChJsonObject.h
@@ -1,52 +1,200 @@
 #ifndef Ch_CPP_JsonObject_h
 #define Ch_CPP_JsonObject_h
 
-#include"../ChBaseObject/ChBaseObject.h"
-
 namespace ChCpp
 {
-	class JsonObject:protected BaseObject
+	class JsonArray;
+	class JsonNumber;
+	class JsonString;
+	class JsonBoolean;
+
+	class JsonObjectBase
 	{
+	public://Set Function//
+
+		virtual bool SetRawData(const std::string& _jsonText) = 0;
+
+	public://Get Function//
+
+		virtual std::string GetRawData()const = 0;
+
+	protected://Is Function//
+
+		bool IsObject(const std::string& _json);
+
+		bool IsArray(const std::string& _json);
+
+		bool IsString(const std::string& _json);
+
+		bool IsNumber(const std::string& _json);
+
+		bool IsBoolean(const std::string& _json);
+	};
+
+	class JsonObject :public JsonObjectBase
+	{
+	public://Set Function//
+
+		bool SetRawData(const std::string& _jsonText)override;
+
+		void SetArray(const std::string& _parameterName,const JsonArray& _value);
+
+		void SetNumber(const std::string& _parameterName, const JsonNumber& _value);
+
+		void SetString(const std::string& _parameterName,const JsonString& _value);
+
+		bool SetBoolean(const std::string& _parameterName, const JsonBoolean& _value);
+
+		void SetObject(const std::string& _parameterName,const JsonObject& _value);
+
+	public: //Get Function//
+
+		std::string GetRawData()const override;
+
+		JsonArray GetArray(const std::string& _parameterName)const;
+
+		JsonNumber GetNumber(const std::string& _parameterName)const;
+
+		JsonString GetString(const std::string& _parameterName)const;
+
+		JsonBoolean GetBoolean(const std::string& _parameterName)const;
+	
+		JsonObject GetObject(const std::string& _parameterName)const;
 
 	private:
 
-		std::map<std::string, ChPtr::Shared<JsonObject>>value;
+		std::map<std::string, ChPtr::Shared<JsonObjectBase>> value;
 
 	};
 
-	class JsonArray :public JsonObject
+	class JsonArray :public JsonObjectBase
 	{
+	public:
+
+		bool SetRawData(const std::string& _jsonText)override;
+
+		void SetValue(unsigned long _num, const JsonObject& _value);
+
+	public:
+
+		std::string GetRawData()const override;
+
+		ChPtr::Shared<JsonObjectBase> GetValue(unsigned long _num)const;
+
+	public:
+
+		void AddObject(ChPtr::Shared<JsonObjectBase> _value);
+
 	private:
 
-		std::vector<ChPtr::Shared<JsonObject>>value;
+		std::vector<ChPtr::Shared<JsonObjectBase>>values;
 
 	};
 
-	class JsonNumber :public JsonObject
+	class JsonNumber :public JsonObjectBase
 	{
+	public://Operator Functions//
+
+
+	public://To BaseClass Operator Functions//
+
+		template<typename BaseType>
+		operator BaseType()const;
+
+		template<typename BaseType>
+		JsonNumber operator = (BaseType& _base);
+
+		template<typename BaseType>
+		JsonNumber operator += (BaseType& _base);
+
+		template<typename BaseType>
+		JsonNumber operator + (BaseType& _base)const;
+
+		template<typename BaseType>
+		JsonNumber operator -= (BaseType& _base);
+
+		template<typename BaseType>
+		JsonNumber operator - (BaseType& _base)const;
+
+		template<typename BaseType>
+		JsonNumber operator *= (BaseType& _base);
+
+		template<typename BaseType>
+		JsonNumber operator * (BaseType& _base)const;
+
+		template<typename BaseType>
+		JsonNumber operator /= (BaseType& _base);
+
+		template<typename BaseType>
+		JsonNumber operator / (BaseType& _base)const;
+
+		template<typename BaseType>
+		JsonNumber operator %= (BaseType& _base);
+
+		template<typename BaseType>
+		JsonNumber operator % (BaseType& _base)const;
+
+
+	public://To String Operator Functions//
+
+		operator JsonString()const;
+		operator std::string()const;
+
+		std::string operator +(JsonNumber& _value)const;
+
+
+	public://Constructor Destructor//
+
+		JsonNumber();
+
+		template<typename BaseType>
+		JsonNumber(BaseType);
+
+	public:
+
+		bool SetRawData(const std::string& _jsonText)override;
+
+	public:
+
+		std::string GetRawData()const override;
 
 	private:
 
-		double value = 0.0;
-
+		long double value;
 	};
 
-	class JsonString :public JsonObject
+	class JsonString :public JsonObjectBase
 	{
+	public:
+
+		JsonString operator +(JsonNumber& _value)const;
+
+	public:
+
+		bool SetRawData(const std::string& _jsonText)override;
+
+	public:
+
+		std::string GetRawData()const override;
 
 	private:
 
-		std::string value = "";
-
+		std::string value;
 	};
 
-	class JsonBoolean :public JsonObject
+	class JsonBoolean :public JsonObjectBase
 	{
+	public:
+
+		bool SetRawData(const std::string& _jsonText)override;
+
+	public:
+
+		std::string GetRawData()const override;
 
 	private:
 
-		bool value = false;;
-
+		bool value;
 	};
 }
 
