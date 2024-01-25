70d95cc2877bc88f7780a22ae2cbdd1a85af1b83
Chronoss0518 <sinyusuke5000@gmail.com>
2024-01-25 23:18:34 +0900

    BlurShaderを利用するクラス作成

---
+++ ChCppLibrary/Direct3D11/ChSampleShader/SpriteShader/ChBasicBlurShader11.h
@@ -0,0 +1,71 @@
+#ifndef Ch_D3D11_SS_BasicBlur_h
+#define Ch_D3D11_SS_BasicBlur_h
+
+#include"ChSampleSpriteShaderBase11.h"
+
+
+namespace ChD3D11
+{
+
+	class Sprite11;
+
+	namespace Shader
+	{
+
+		class BasicBlurShader11 final :public SampleSpriteShaderBase11
+		{
+		public:
+
+			virtual ~BasicBlurShader11()
+			{
+				Release();
+			}
+
+		public://Init And Release//
+
+			void Init(ID3D11Device* _device)override;
+
+			void Release()override;
+
+		protected://Init And Release//
+
+			void InitPixelShader()override;
+
+		public://Set Functions//
+
+			inline void SetBlurPower(const int& _blurPower) { blurData.SetBlurPower(_blurPower); }
+
+			void SetGameWindowSize(const ChVec2& _size) { blurData.SetGameWindowSize(_size); }
+
+		public://Get Functions//
+
+			inline int GetBlurPower() { return blurData.GetBlurPower(); }
+
+			inline ChVec2 GetGameWindowSize() { return blurData.GetGameWindowSize(); }
+
+		public://Other Functions//
+
+			//í èÌï`âÊ//
+			void Draw(
+				TextureBase11& _tex
+				, Sprite11& _sprite
+				, const ChMat_11& _mat = ChMat_11());
+
+			//í èÌï`âÊ//
+			void Draw(
+				TextureBase11& _tex
+				, Sprite11& _sprite
+				, const ChVec4& _baseColor
+				, const ChMat_11& _mat = ChMat_11());
+
+		private://Member Value//
+
+			CB::CBBlur11 blurData;
+
+		};
+	}
+}
+
+
+
+#endif
