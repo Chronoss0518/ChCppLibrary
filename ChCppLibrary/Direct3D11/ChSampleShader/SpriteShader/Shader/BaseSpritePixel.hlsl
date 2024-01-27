190cb6eaab8553ba5b5eec04b9cfb1e95029af9b
Chronoss0518 <sinyusuke5000@gmail.com>
2024-01-27 12:07:07 +0900

    BaseColorが計算に加わっていなかったため修正

--- ChCppLibrary/Direct3D11/ChSampleShader/SpriteShader/Shader/BaseSpritePixel.hlsl
+++ ChCppLibrary/Direct3D11/ChSampleShader/SpriteShader/Shader/BaseSpritePixel.hlsl
@@ -12,7 +12,7 @@ float4 main(VS_OUT _in) : SV_Target0
 {
 	float4 res = _in.color;
 	res *= GetBaseTextureColor(_in.uv);
-
+    res *= baseColor;
 	//Out = BaseColor;
 
 	clip(res.a < 0.01f ? -1 : 1);
