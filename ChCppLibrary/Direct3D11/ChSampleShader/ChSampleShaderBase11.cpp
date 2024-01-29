874bbf2fb65e9dfe2cf80559dc78723611727796
Chronoss0518 <sinyusuke5000@gmail.com>
2024-01-29 13:37:05 +0900

    AlphaTestを2つ目以降のRenderTargetに追加するように修正

--- ChCppLibrary/Direct3D11/ChSampleShader/ChSampleShaderBase11.cpp
+++ ChCppLibrary/Direct3D11/ChSampleShader/ChSampleShaderBase11.cpp
@@ -30,8 +30,8 @@ void SampleShaderBase11::Init(ID3D11Device* _device)
 
 	{
 		D3D11_BLEND_DESC desc;
-		desc.AlphaToCoverageEnable = false;
-		desc.IndependentBlendEnable = false;
+		desc.AlphaToCoverageEnable = true;
+		desc.IndependentBlendEnable = true;
 		desc.RenderTarget[0].BlendEnable = true;
 		desc.RenderTarget[0].SrcBlend = D3D11_BLEND::D3D11_BLEND_SRC_ALPHA;
 		desc.RenderTarget[0].DestBlend = D3D11_BLEND::D3D11_BLEND_INV_SRC_ALPHA;
