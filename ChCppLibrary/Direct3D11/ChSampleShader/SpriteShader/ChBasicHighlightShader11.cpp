e35674625abb951396c6ef685cbba2ce4f42173e
Chronoss0518 <sinyusuke5000@gmail.com>
2024-04-23 18:46:52 +0900

    対象Bitに対しての計算方法を修正

--- ChCppLibrary/Direct3D11/ChSampleShader/SpriteShader/ChBasicHighlightShader11.cpp
+++ ChCppLibrary/Direct3D11/ChSampleShader/SpriteShader/ChBasicHighlightShader11.cpp
@@ -27,12 +27,12 @@ void BasicHighlightShader11::Init(ID3D11Device* _device)
 		desc.AlphaToCoverageEnable = false;
 		desc.IndependentBlendEnable = false;
 		desc.RenderTarget[0].BlendEnable = true;
-		desc.RenderTarget[0].SrcBlend = D3D11_BLEND::D3D11_BLEND_INV_DEST_COLOR;
+		desc.RenderTarget[0].SrcBlend = D3D11_BLEND::D3D11_BLEND_SRC_COLOR;
 		desc.RenderTarget[0].DestBlend = D3D11_BLEND::D3D11_BLEND_DEST_COLOR;
 		desc.RenderTarget[0].BlendOp = D3D11_BLEND_OP::D3D11_BLEND_OP_ADD;
-		desc.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND::D3D11_BLEND_INV_DEST_COLOR;
-		desc.RenderTarget[0].DestBlendAlpha = D3D11_BLEND::D3D11_BLEND_DEST_COLOR;
-		desc.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP::D3D11_BLEND_OP_ADD;
+		desc.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND::D3D11_BLEND_SRC_COLOR;
+		desc.RenderTarget[0].DestBlendAlpha = D3D11_BLEND::D3D11_BLEND_SRC_COLOR;
+		desc.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP::D3D11_BLEND_OP_MAX;
 		desc.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE::D3D11_COLOR_WRITE_ENABLE_ALL;
 
 
