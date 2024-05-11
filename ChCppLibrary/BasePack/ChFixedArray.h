1f143f07329e2d7648f4059b690b260052393712
Chronoss0518 <sinyusuke5000@gmail.com>
2024-05-11 21:28:22 +0900

    固定長配列を追加

--- ChCppLibrary/BasePack/ChFixedArray.h
+++ ChCppLibrary/BasePack/ChFixedArray.h
@@ -5,12 +5,37 @@
 
 namespace ChArray
 {
+	//Normal Fixed Length Array//
+	//’Êí‚ÌŒÅ’è’·”z—ñ//
 	template<typename T,unsigned long Array>
-	class FixedArray : public ArrayBase
+	class FixedArray : public ArrayBase<T>
 	{
+	public:
+
+		inline FixedArray()
+		{
+			ArrayBase<T>::SetCount(Array);
+		}
+
+		inline ~FixedArray()
+		{
+			ArrayBase<T>::SetCount(0);
+		}
+
+	public:
+
+		inline T& GetItem(unsigned long _num)override
+		{
+			unsigned long num = _num;
+			if (Array - 1<= num)num = Array - 1;
+			return item[num];
+		}
+
+	private:
+
+		T item[Array];
 
 	};
-
 }
 
 #endif
