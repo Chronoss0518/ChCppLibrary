367d29833ba1a713e904f3de58d482034fddeec1
Chronoss0518 <sinyusuke5000@gmail.com>
2024-05-11 19:25:34 +0900

    固定長配列の自作クラスの作成準備

---
+++ ChCppLibrary/BasePack/ChFixedArray.h
@@ -0,0 +1,16 @@
+#ifndef Ch_CPP_FixedArray_h
+#define Ch_CPP_FixedArray_h
+
+#include"ChArrayBase.h"
+
+namespace ChArray
+{
+	template<typename T,unsigned long Array>
+	class FixedArray : public ArrayBase
+	{
+
+	};
+
+}
+
+#endif
