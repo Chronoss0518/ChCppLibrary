8c4347e90aae9793bf79dcf00f7cfba104b5ba3d
Chronoss0518 <sinyusuke5000@gmail.com>
2024-07-20 17:10:24 +0900

    char*のみの機能のため、stringを利用しないように修正

--- ChCppLibrary/ClassParts/ChCPClassNameGetter.h
+++ ChCppLibrary/ClassParts/ChCPClassNameGetter.h
@@ -3,41 +3,40 @@
 #ifndef Ch_CPP_CP_CNGet_h
 #define Ch_CPP_CP_CNGet_h
 
+#ifdef CRT
+#include<string>
+#endif
+
 namespace ChCp
 {
 	//継承して利用する//
 	//自身のクラスネーム及びクラス自身の型を取得できる//
+	template<class T>
 	class ClassNameGetter
 	{
 	public:
 
 		///////////////////////////////////////////////////////////////////////////////////
 		//GetFunction//
-
-		template<class T>
-		inline std::string GetName()
+		inline char* GetName()
 		{
-			std::string tmp;
+			char* tmp = nullptr;
 
 #ifdef __GNUC__
-
 			tmp = typeid(T).name();
-
 #else
-
 			tmp = typeid(T).raw_name();
-
 #endif
 			return tmp;
 		}
-
 	protected:
 
 		ClassNameGetter() {}
 
 	};
 
-	using CNGetter = ClassNameGetter;
+	template<class T>
+	using CNGetter = ClassNameGetter<T>;
 
 }
 
