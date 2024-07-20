36e767694b1fcb1b1a4231830a4a411142391af5
Chronoss0518 <sinyusuke5000@gmail.com>
2024-07-20 17:09:58 +0900

    #ifdef CRTのフラグを一回のみに修正

--- ChCppLibrary/BasePack/ChPtr.h
+++ ChCppLibrary/BasePack/ChPtr.h
@@ -28,91 +28,69 @@ namespace ChPtr
 	//SharedPtrの簡略版//
 	template<class T>
 	using Shared = std::shared_ptr<T>;
-#endif
 
-#ifdef CRT
 	//UniquePtrの簡略版//
 	template<class T>
 	using Unique = std::unique_ptr<T>;
-#endif
 
-#ifdef CRT
 	//WeakPtrの簡略版//
 	template<class T>
 	using Weak = std::weak_ptr<T>;
-#endif
 
-#ifdef CRT
 	//SharedPtr用ダウンキャスト//
 	template<class C, class C2>
 	static inline auto SharedSafeCast(Shared<C2> _sPtr)CH_SAFE_CAST_TRUE(C2,C)
 	{
 		return std::dynamic_pointer_cast<C, C2>(_sPtr);
 	}
-#endif
 
-#ifdef CRT
 	template<class C, class C2>
 	static inline auto SharedSafeCast(Shared<C2> _sPtr)CH_SAFE_CAST_FALSE(C2,C)
 	{
 		return _sPtr;
 	}
-#endif
 
-#ifdef CRT
 	//*Ptr用ダウンキャスト//
 	template<class C, class C2>
 	static inline auto SafeCast(C2*_ptr)CH_SAFE_CAST_TRUE(C2, C)
 	{
 		return dynamic_cast<C*>(_ptr);
 	}
-#endif
 
-#ifdef CRT
 	template<class C, class C2>
 	static inline auto SafeCast(C2* _ptr)CH_SAFE_CAST_FALSE(C2, C)
 	{
 		return (_ptr);
 	}
-#endif
 
-#ifdef CRT
 	//クラスがNULLまたはnullptrかをチェックする関数//
 	template<class C>
 	static inline auto NullCheck(const C& _class)CH_POINTER_TEST(C)
 	{
 		return (_class == NULL || _class == nullptr);
 	}
-#endif
 
-#ifdef CRT
 	//クラスがNULLとnullptrのどちらでもないかをチェックする関数//
 	template<class C>
 	static inline auto NotNullCheck(const C& _class)CH_POINTER_TEST(C)
 	{
 		return (_class != NULL && _class != nullptr);
 	}
-#endif
 
-#ifdef CRT
 	//make_sharedを短縮するための関数//
 	template<class T, class... _Types>
 	static inline Shared<T> Make_S(_Types&&... _args)
 	{
 		return std::make_shared<T>(_args...);
 	}
-#endif
 
-#ifdef CRT
 	//make_uniqueを短縮するための関数//
 	template<class T, class... _Types>
 	static inline Unique<T> Make_U(_Types&&... _args)
 	{
 		return std::make_unique<T>(_args...);
 	}
-#endif
 
-#ifdef CRT
 	//move_ptrを短縮するための関数//
 	template<class T>
 	static inline T Move(T _obj)
