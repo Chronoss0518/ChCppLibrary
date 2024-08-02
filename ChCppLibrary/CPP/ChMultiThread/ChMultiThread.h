ce8e2b7c5fb6c40bb3aa0ac3e678657065be77d4
Chronoss0518 <sinyusuke5000@gmail.com>
2024-08-02 21:08:00 +0900

    MultiThreadからCRTを排除

--- ChCppLibrary/CPP/ChMultiThread/ChMultiThread.h
+++ ChCppLibrary/CPP/ChMultiThread/ChMultiThread.h
@@ -2,42 +2,40 @@
 #ifndef Ch_CPP_MuTh_h
 #define Ch_CPP_MuTh_h
 
-namespace std
-{
-	class thread;
-}
+#ifdef CRT
+#include<thread>
+#include<functional>
+#endif
 
 namespace ChCpp
 {
 	//void(void)の関数用マルチスレッドを実行するクラス//
 	class MultiThread :public ChCp::Initializer
 	{
-	public:
-
-		///////////////////////////////////////////////////////////////////////////////////
-		//ConstructerDestructer//
+	public://ConstructerDestructer//
 
 		MultiThread() {}
 
+#ifdef CRT
 		MultiThread(const std::function<void(void)> _func);
+#endif
 
 		~MultiThread() { Release(); }
+	public://InitAndRelease//
 
-		///////////////////////////////////////////////////////////////////////////////////
-		//InitAndRelease//
-
+#ifdef CRT
 		//マルチスレッドで動かしたい関数をセットする//
 		void Init(const std::function<void(void)> _func);
+#endif
 
 		void Release();
 
-		///////////////////////////////////////////////////////////////////////////////////
-		//IsFunction//
+	public://IsFunction//
 
 		//マルチスレッドで動いている関数が終わったかの判定//
-		bool IsEndFunc() { return endFlg; }
+		inline bool IsEndFunc() { return endFlg; }
 
-		///////////////////////////////////////////////////////////////////////////////////
+	public://Other Functions//
 
 		//マルチスレッドで動いている関数が終わるまで待つ//
 		void Join();
@@ -45,19 +43,70 @@ namespace ChCpp
 		//同じ関数を再度走らせる//
 		void ReRun();
 
-	protected:
+	protected://Other Functions//
 
 		void Function();
 
-		std::thread thread;
+	protected://Member Value//
 
+#ifdef CRT
+		std::thread thread;
 		std::function<void(void)> func = nullptr;
+#endif
 
 		bool endFlg = true;
-
 	};
+}
 
+#ifdef CRT
+
+ChCpp::MultiThread::MultiThread(const std::function<void(void)> _func) { Init(_func); }
+
+void ChCpp::MultiThread::Init(const std::function<void(void)> _func)
+{
+	if (!endFlg)return;
+
+	if (thread.joinable())
+		thread.detach();
+
+	func = _func;
+
+	ReRun();
+
+	SetInitFlg(true);
+}
+
+void ChCpp::MultiThread::ReRun()
+{
+	if (!endFlg)return;
+
+	endFlg = false;
+
+	thread = std::thread([&] {Function(); });
 
 }
+
+void ChCpp::MultiThread::Release()
+{
+	if (!IsInit())return;
+
+	if (!thread.joinable())return;
+
+	thread.join();
+
+	SetInitFlg(false);
+}
+
+void ChCpp::MultiThread::Join() { thread.join(); }
+
+void ChCpp::MultiThread::Function()
+{
+	func();
+
+	endFlg = true;
+}
+
+
+#endif
 
 #endif
