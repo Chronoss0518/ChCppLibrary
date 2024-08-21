118d97b4d9ddb6e84cf2e2e75a1ca454a86554dd
Chronoss0518 <sinyusuke5000@gmail.com>
2024-08-21 19:40:10 +0900

    RadioButtonからCRTを排除

--- ChCppLibrary/WindowsObject/RadioButton/ChWinRadioButton.h
+++ ChCppLibrary/WindowsObject/RadioButton/ChWinRadioButton.h
@@ -5,218 +5,234 @@
 
 #include"../WindObject/ChWindObject.h"
 
-namespace ChSystem
-{
-	class Windows;
-}
-
 namespace ChWin
 {
 
-	class RadioButton : public WindObject
+	class RadioButtonA : public WindObjectA
 	{
 	public:
 
-		virtual ~RadioButton()
+		virtual ~RadioButtonA()
 		{
 			Release();
 		}
 
+	public://Create Functions//
+
+		//TextBox�̍쐬//
+		void Create(
+			HINSTANCE _hIns,
+			const char* _startText,
+			const ChINTPOINT& _pos,
+			const ChINTPOINT& _size,
+			const bool _glpFlg,
+			const HWND _parentHandl = nullptr);
+
+		//TextBox�̍쐬//
+		void Create(
+			HINSTANCE _hIns,
+			const char* _startText,
+			const int _x,
+			const int _y,
+			const int _w,
+			const int _h,
+			const bool _glpFlg,
+			const HWND _parentHandl = nullptr);
+
+		//TextBox�̍쐬//
+		void Create(
+			const char* _startText,
+			const ChINTPOINT& _pos,
+			const ChINTPOINT& _size,
+			const bool _glpFlg,
+			const WindObjectA& _parentWind);
+
+		//TextBox�̍쐬//
+		void Create(
+			const char* _startText,
+			const int _x,
+			const int _y,
+			const int _w,
+			const int _h,
+			const bool _glpFlg,
+			const WindObjectA& _parentWind);
 
 	public:
 
-		///////////////////////////////////////////////////////////////////////////////////////
-
-		//TextBox�̍쐬//
-		void CreateA(
-			HINSTANCE _hIns,
-			const std::string& _startText,
-			const ChINTPOINT& _pos,
-			const ChINTPOINT& _size,
-			const bool _glpFlg,
-			const HWND _parentHandl = nullptr);
-
-		//TextBox�̍쐬//
-		void CreateA(
-			HINSTANCE _hIns,
-			const std::string& _startText,
-			const int _x,
-			const int _y,
-			const int _w,
-			const int _h,
-			const bool _glpFlg,
-			const HWND _parentHandl = nullptr);
-
-		//TextBox�̍쐬//
-		void CreateA(
-			const std::string& _startText,
-			const ChINTPOINT& _pos,
-			const ChINTPOINT& _size,
-			const bool _glpFlg,
-			const WindObject& _parentWind);
-
-		//TextBox�̍쐬//
-		void CreateA(
-			const std::string& _startText,
-			const int _x,
-			const int _y,
-			const int _w,
-			const int _h,
-			const bool _glpFlg,
-			const WindObject& _parentWind);
-
-		//TextBox�̍쐬//
-		void CreateW(
-			HINSTANCE _hIns,
-			const std::wstring& _startText,
-			const ChINTPOINT& _pos,
-			const ChINTPOINT& _size,
-			const bool _glpFlg,
-			const HWND _parentHandl = nullptr);
-
-		//TextBox�̍쐬//
-		void CreateW(
-			HINSTANCE _hIns,
-			const std::wstring& _startText,
-			const int _x,
-			const int _y,
-			const int _w,
-			const int _h,
-			const bool _glpFlg,
-			const HWND _parentHandl = nullptr);
-
-		//TextBox�̍쐬//
-		void CreateW(
-			const std::wstring& _startText,
-			const ChINTPOINT& _pos,
-			const ChINTPOINT& _size,
-			const bool _glpFlg,
-			const WindObject& _parentWind);
-
-		//TextBox�̍쐬//
-		void CreateW(
-			const std::wstring& _startText,
-			const int _x,
-			const int _y,
-			const int _w,
-			const int _h,
-			const bool _glpFlg,
-			const WindObject& _parentWind);
-
-		void Create(
-			HINSTANCE _hIns,
-			const
-#ifdef UNICODE
-			std::wstring
-#else
-			std::string
-#endif
-			_startText,
-			const ChINTPOINT& _pos,
-			const ChINTPOINT& _size,
-			const bool _glpFlg,
-			const HWND _parentHandl = nullptr
-		)
+		void SetCheck()
 		{
-
-#ifdef UNICODE
-			CreateW(_hIns, _startText, _pos, _size, _glpFlg, _parentHandl);
-#else
-			CreateA(_hIns, _startText, _pos, _size, _glpFlg, _parentHandl);
-#endif
+			SendMessageA(hWnd, BM_SETCHECK, BST_CHECKED, 0);
 		}
 
-		void Create(
-			HINSTANCE _hIns,
-			const
-#ifdef UNICODE
-			std::wstring
-#else
-			std::string
-#endif
-			_startText,
-			const int _x,
-			const int _y,
-			const int _w,
-			const int _h,
-			const bool _glpFlg,
-			const HWND _parentHandl = nullptr
-		)
+		void SetUnCheck()
 		{
-
-#ifdef UNICODE
-			CreateW(_hIns, _startText, ChINTPOINT(_x, _y), ChINTPOINT(_w, _h), _glpFlg, _parentHandl);
-#else
-			CreateA(_hIns, _startText, ChINTPOINT(_x, _y), ChINTPOINT(_w, _h), _glpFlg, _parentHandl);
-#endif
+			SendMessageA(hWnd, BM_SETCHECK, BST_UNCHECKED, 0);
 		}
 
-		void Create(
-			const
-#ifdef UNICODE
-			std::wstring
-#else
-			std::string
-#endif
-			_startText,
-			const ChINTPOINT& _pos,
-			const ChINTPOINT& _size,
-			const bool _glpFlg,
-			const WindObject& _parentWind
-		)
-		{
-
-#ifdef UNICODE
-			CreateW(_startText, _pos, _size, _glpFlg, _parentWind);
-#else
-			CreateA(_startText, _pos, _size, _glpFlg, _parentWind);
-#endif
-		}
-
-		void Create(
-			const
-#ifdef UNICODE
-			std::wstring
-#else
-			std::string
-#endif
-			_startText,
-			const int _x,
-			const int _y,
-			const int _w,
-			const int _h,
-			const bool _glpFlg,
-			const WindObject& _parentWind
-		)
-		{
-
-#ifdef UNICODE
-			CreateW(_startText, ChINTPOINT(_x, _y), ChINTPOINT(_w, _h), _glpFlg, _parentWind);
-#else
-			CreateA(_startText, ChINTPOINT(_x, _y), ChINTPOINT(_w, _h), _glpFlg, _parentWind);
-#endif
-		}
-
-	public:
-
-		///////////////////////////////////////////////////////////////////////////////////////
-		//IsFunction//
+	public://IsFunction//
 
 		bool isSelection()
 		{
-			return (SendMessage(GethWnd(), BM_GETCHECK, 0, 0) == BST_CHECKED);
+			return (SendMessageA(GethWnd(), BM_GETCHECK, 0, 0) == BST_CHECKED);
 		}
-
-		///////////////////////////////////////////////////////////////////////////////////////
 
 	protected:
 
+	};
 
+	class RadioButtonW : public WindObjectW
+	{
+	public:
 
+		virtual ~RadioButtonW()
+		{
+			Release();
+		}
+
+	public://Create Functions//
+
+		//TextBox�̍쐬//
+		void Create(
+			HINSTANCE _hIns,
+			const wchar_t* _startText,
+			const ChINTPOINT& _pos,
+			const ChINTPOINT& _size,
+			const bool _glpFlg,
+			const HWND _parentHandl = nullptr);
+
+		//TextBox�̍쐬//
+		void Create(
+			HINSTANCE _hIns,
+			const wchar_t* _startText,
+			const int _x,
+			const int _y,
+			const int _w,
+			const int _h,
+			const bool _glpFlg,
+			const HWND _parentHandl = nullptr);
+
+		//TextBox�̍쐬//
+		void Create(
+			const wchar_t* _startText,
+			const ChINTPOINT& _pos,
+			const ChINTPOINT& _size,
+			const bool _glpFlg,
+			const WindObjectW& _parentWind);
+
+		//TextBox�̍쐬//
+		void Create(
+			const wchar_t* _startText,
+			const int _x,
+			const int _y,
+			const int _w,
+			const int _h,
+			const bool _glpFlg,
+			const WindObjectW& _parentWind);
+
+	public:
+
+		void SetCheck()
+		{
+			SendMessageW(hWnd, BM_SETCHECK, BST_CHECKED, 0);
+		}
+
+		void SetUnCheck()
+		{
+			SendMessageW(hWnd, BM_SETCHECK, BST_UNCHECKED, 0);
+		}
+
+	public://IsFunction//
+
+		bool isSelection()
+		{
+			return (SendMessageW(GethWnd(), BM_GETCHECK, 0, 0) == BST_CHECKED);
+		}
+
+	protected:
 
 	};
 
+
+	using RadioButton =
+#ifdef UNICODE
+		RadioButtonW;
+#else
+		RadioButtonA;
+#endif
 }
+
+#ifdef CRT
+
+void ChWin::RadioButtonA::Create(
+	HINSTANCE _hIns,
+	const char* _startText,
+	const ChINTPOINT& _pos,
+	const ChINTPOINT& _size,
+	const bool _glpFlg,
+	const HWND _parentHandl)
+{
+	Release();
+
+	auto pos = _pos;
+	auto size = _size;
+	pos.val.Abs();
+	size.val.Abs();
+
+	WindCreater creater(_hIns);
+
+	creater.SetEXStyle(WS_EX_CLIENTEDGE);
+
+	creater.SetWindStyle(
+		WS_VISIBLE
+		| WS_CHILD
+		| (_glpFlg ? 0 : WS_GROUP)
+		| BS_AUTORADIOBUTTON
+		| WS_CLIPCHILDREN);
+
+	creater.SetInitPosition(pos.x, pos.y);
+	creater.SetInitSize(size.w, size.h);
+	creater.SetParentWind(_parentHandl);
+
+	hWnd = creater.Create(_startText, "BUTTON");
+	CreateEnd(true);
+}
+
+void ChWin::RadioButtonW::Create(
+	HINSTANCE _hIns,
+	const wchar_t* _startText,
+	const ChINTPOINT& _pos,
+	const ChINTPOINT& _size,
+	const bool _glpFlg,
+	const HWND _parentHandl)
+{
+	Release();
+
+	auto pos = _pos;
+	auto size = _size;
+	pos.val.Abs();
+	size.val.Abs();
+
+	WindCreater creater(_hIns);
+
+	creater.SetEXStyle(WS_EX_CLIENTEDGE);
+
+	creater.SetWindStyle(
+		WS_VISIBLE
+		| WS_CHILD
+		| (_glpFlg ? 0 : WS_GROUP)
+		| BS_AUTORADIOBUTTON
+		| WS_CLIPCHILDREN);
+
+	creater.SetInitPosition(pos.x, pos.y);
+	creater.SetInitSize(size.w, size.h);
+	creater.SetParentWind(_parentHandl);
+
+	hWnd = creater.Create(_startText, L"BUTTON");
+	CreateEnd(true);
+}
+
+
+#endif
 
 #endif
 
