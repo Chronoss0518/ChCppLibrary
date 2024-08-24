7883c81b9947b4dcf5a178aacd67741f2b1b2957
Chronoss0518 <sinyusuke5000@gmail.com>
2024-08-24 21:50:24 +0900

    include時に判別するDefineを変更

--- ChCppLibrary/BaseIncluder/ChD3D9I.h
+++ ChCppLibrary/BaseIncluder/ChD3D9I.h
@@ -1,7 +1,8 @@
+#ifdef _WINDOWS_
+
 #ifndef Ch_BD9I_h
 #define Ch_BD9I_h
 
-#ifdef _WINDEF_
 #include<dsound.h>
 
 #pragma comment(lib,"d3d9.lib")
@@ -16,7 +17,7 @@
 #include"../Direct3D9/ChPackDate/ChQuaternion_9.h"
 #include"../Direct3D9/ChPackDate/ChMaterial9.h"
 #include"../Direct3D9/ChDirectX9Controller/ChDirectX9Controller.h"
-
 #endif
 
 #endif
+
