b02dfb68893f033745c653d4de44c9ee1241fc85
Chronoss0518 <sinyusuke5000@gmail.com>
2024-09-04 18:58:05 +0900

    ChMeshクラス群からCRTを排除

--- ChCppLibrary/Direct3D9/ChMesh/ChBaseMesh9.h
+++ ChCppLibrary/Direct3D9/ChMesh/ChBaseMesh9.h
@@ -2,11 +2,28 @@
 #ifndef Ch_D3D9_BMe_h
 #define Ch_D3D9_BMe_h
 
+#ifdef CRT
+
+#include<string>
+#include<vector>
+#include<map>
+#include<memory>
+#include<functional>
+
+#endif
+
+#include"../../BasePack/ChPtr.h"
+
+#include"../../BaseIncluder/ChD3D9I.h"
+
+#include"../ChTexture/ChBaseTexture9.h"
+#include"../ChTexture/TexIncluder9.h"
 
 namespace ChTex
 {
 	typedef class BaseTexture9 Texture9;
 }
+
 
 class ChObjectController9;
 
@@ -16,8 +33,6 @@ namespace ChMesh
 	class MeshManager9;
 	class MeshList9;
 
-#ifndef _ChMesh9
-#define _ChMesh9
 
 	//メッシュの頂点データ//
 	struct MeshVertex9
@@ -34,8 +49,6 @@ namespace ChMesh
 		ChVec3_9 centerPos;
 	};
 
-#endif
-
 	//D3DXMeshを操る基底クラス//
 	typedef class BaseMesh9
 	{
@@ -46,54 +59,65 @@ namespace ChMesh
 			Release();
 		}
 
-	public:
-
-		///////////////////////////////////////////////////////////////////////////////////
-		//InitAndRelease//
+	public://Init And Release//
 
 		virtual void Release();
 
-		///////////////////////////////////////////////////////////////////////////////////
-		//SetFunction//
+	public://Set Functions//
 
 		virtual void SetSkin(){}
 
-		void SetMaterialName(const std::string& _fileName);
-
-		inline void SetMaterialCol(
-			const unsigned long _Num
-			,const ChVec4& _Dif) 
+#ifdef CRT
+		void SetMaterialName(const std::string& _fileName)
 		{
-			if (material.size() <= _Num)return;
+			ChCpp::CharFile file;
+			file.FileOpen(_fileName);
 
-			material[_Num]->Diffuse.a = _Dif.a;
-			material[_Num]->Diffuse.r = _Dif.r;
-			material[_Num]->Diffuse.g = _Dif.g;
-			material[_Num]->Diffuse.b = _Dif.b;
+			std::string tmpStr;
+			tmpStr = file.FileReadText();
+
+			std::string tmpMateName = "Material ";
+
+			size_t size = 0;
+
+			unsigned int i = 0;
+
+			while ((size = tmpStr.find(tmpMateName, size)) != std::string::npos)
+			{
+				size += tmpMateName.length();
+				size_t tmpNum = tmpStr.find("{", size);
+				if (tmpNum == std::string::npos)break;
+				tmpNum -= (size + 1);
+				material[i]->name = tmpStr.substr(size, tmpNum);
+				size += tmpNum;
+				i++;
+			}
 
 		}
 
-		inline void SetMaterialMatrix(
-			const unsigned long _Num
-			, const ChLMat& _Mat)
-		{
-			if (material.size() <= _Num)return;
+#endif
 
-			material[_Num]->mat = _Mat;
+		void SetMaterialCol(
+			const unsigned long _Num,
+			const ChVec4& _Dif);
 
-		}
+		void SetMaterialMatrix(
+			const unsigned long _Num,
+			const ChLMat& _Mat);
 
-		///////////////////////////////////////////////////////////////////////////////////
-		//GetFunction//
+		void SetOffsetVertex(const ChVec3_9& _vertex);
+
+	public://Get Functions//
 
 		inline const LPD3DXMESH GetMesh() const { return mesh; }
 
+#ifdef CRT
 		inline std::vector<ChPtr::Shared<ChVec3_9>> GetVertexList()const
 		{ 
 			return offsetVertexList; 
 		}
 
-		inline std::vector<ChPtr::Shared<ChMaterial_9>> GetMaterials() const { return material; }
+		inline std::vector<ChPtr::Shared<ChMaterialA_9>> GetMaterials() const { return material; }
 
 		inline std::vector<ChPtr::Shared<ChTex::BaseTexture9>> GetTex() const { return texList; }
 
@@ -102,34 +126,75 @@ namespace ChMesh
 		inline std::vector<ChPtr::Shared<MeshFace9>> GetFace() const { return easyFaceList; }
 		
 		virtual inline ChMat_9 GetBoneMat(const std::string& _str){ return ChMat_9(); }
+#endif
 
-		///////////////////////////////////////////////////////////////////////////////////
+		ChVec3_9 GetOffsetVertex(unsigned long _num);
 
-		void CreateEasyFaceList();
+		unsigned long GetMaterialCount();
 
-		///////////////////////////////////////////////////////////////////////////////////
+		ChMaterialA_9 GetMaterial(unsigned long _num);
 
-		virtual void Draw(
-			const ChMat_9& _mat
-			, const LPDIRECT3DDEVICE9& _dev
-			, const long _subNum = -1);
+		ChTex::BaseTexture9* GetTexture(unsigned long _num);
 
-		///////////////////////////////////////////////////////////////////////////////////
+	public://Create Functions//
+
+#ifdef CRT
+
+		void CreateEasyFaceList()
+		{
+			MeshVertex9* meshData = nullptr;
+
+			WORD* p = nullptr;
+
+			mesh->LockIndexBuffer(0, (LPVOID*)&p);
+			mesh->LockVertexBuffer(0, (LPVOID*)&meshData);
+
+			for (unsigned long faceNum = 0; faceNum < mesh->GetNumFaces(); faceNum++)
+			{
+
+				auto meshFace = ChPtr::Make_S<MeshFace9>();
+
+				CreateEasyFace(*meshFace, faceNum, meshData, p);
+
+				easyFaceList.push_back(meshFace);
+			}
+
+			mesh->UnlockIndexBuffer();
+			mesh->UnlockVertexBuffer();
+		}
 
 		void CreateMesh(
-			const std::string& _fileName
-			, const std::string& _pathName
-			, const LPDIRECT3DDEVICE9& _dev);
+			const std::string& _fileName,
+			const std::string& _pathName,
+			const LPDIRECT3DDEVICE9& _dev)
+		{
+			OpenFile(_fileName, _pathName, _dev);
 
-		///////////////////////////////////////////////////////////////////////////////////
+			SetOffsetVertex();
+		}
 
-		static ChPtr::Shared<BaseMesh9> MeshType(
-			const std::string& _fileName);
+#endif
 
-		///////////////////////////////////////////////////////////////////////////////////
+	protected:
 
-		static ChPtr::Shared<BaseMesh9> SkinMeshType(
-			const std::string& _fileName);
+		void CreateEasyFace(MeshFace9& _out, unsigned long _faceNum,MeshVertex9* _meshData, WORD* _p);
+
+	public://Draw Functions//
+
+		virtual void Draw(
+			const ChMat_9& _mat,
+			const LPDIRECT3DDEVICE9& _dev,
+			const long _subNum = -1);
+
+	public://Static Functions//
+
+#ifdef CRT
+		
+		static ChPtr::Shared<BaseMesh9> MeshType(const std::string& _fileName);
+
+		static ChPtr::Shared<BaseMesh9> SkinMeshType(const std::string& _fileName);
+
+#endif
 
 		friend MeshManager9;
 		friend MeshList9;
@@ -137,48 +202,122 @@ namespace ChMesh
 
 	protected:
 
-		///////////////////////////////////////////////////////////////////////////////////
-
+#ifdef CRT
 		virtual void OpenFile(
-			const std::string& _fileName
-			, const std::string& _pathName
-			, const LPDIRECT3DDEVICE9& _dev) {}
+			const std::string& _fileName,
+			const std::string& _pathName,
+			const LPDIRECT3DDEVICE9& _dev) {}
+#endif
 
-		///////////////////////////////////////////////////////////////////////////////////
-		//InsFunction//
+	public://Ins Functions//
 
 		virtual LPD3DXMESH InsMesh() { return mesh; }
 
-		std::vector<ChPtr::Shared<ChMaterial_9>>& InsMaterials() { return material; }
+#ifdef CRT
+		std::vector<ChPtr::Shared<ChMaterialA_9>>& InsMaterials() { return material; }
 
 		std::vector<ChPtr::Shared<ChTex::BaseTexture9>>& InsTex() { return texList; }
 
 		std::vector<ChPtr::Shared<ChTex::BaseTexture9>>& InsNormalTex() { return normalTex; }
 
 		std::vector<ChPtr::Shared<MeshFace9>>& InsFace() { return easyFaceList; }
-
-		///////////////////////////////////////////////////////////////////////////////////
+#endif
 
 		virtual void SetOffsetVertex();
 
-		///////////////////////////////////////////////////////////////////////////////////
+	protected://Member Method//
 
 		LPD3DXMESH mesh = nullptr;//メッシュの形状//
-		std::vector<ChPtr::Shared<ChMaterial_9>> material;//マテリアルの内容//
+
+		size_t vertexNum = 0;
+
+#ifdef CRT
 		std::vector<ChPtr::Shared<ChTex::BaseTexture9>> texList;//テクスチャーの内容//
+		std::vector<ChPtr::Shared<ChMaterialA_9>> material;//マテリアルの内容//
 		std::vector<ChPtr::Shared<ChTex::BaseTexture9>> normalTex;//法線マッピング//
 
 		std::vector<ChPtr::Shared<ChVec3_9>>optimalVertexList;//最適化した頂点//
 		std::vector<ChPtr::Shared<MeshFace9>>easyFaceList;//最適化した頂点//
 
 		std::vector<ChPtr::Shared<ChVec3_9>>offsetVertexList;//初期の頂点位置//
-		size_t vertexNum = 0;
-
+#endif
 
 	}Mesh9;
+}
 
 
+
+#ifdef CRT
+
+void ChMesh::BaseMesh9::SetOffsetVertex(const ChVec3_9& _vertex)
+{
+	auto&& ver = ChPtr::Make_S<ChVec3_9>();
+	*ver = _vertex;
+	offsetVertexList.push_back(ver);
+}
+
+void ChMesh::BaseMesh9::SetMaterialCol(
+	const unsigned long _Num,
+	const ChVec4& _Dif)
+{
+	if (material.size() <= _Num)return;
+
+	material[_Num]->Diffuse.a = _Dif.a;
+	material[_Num]->Diffuse.r = _Dif.r;
+	material[_Num]->Diffuse.g = _Dif.g;
+	material[_Num]->Diffuse.b = _Dif.b;
+}
+
+void ChMesh::BaseMesh9::SetMaterialMatrix(
+	const unsigned long _Num,
+	const ChLMat& _Mat)
+{
+	if (material.size() <= _Num)return;
+
+	material[_Num]->mat = _Mat;
+}
+
+ChVec3_9 ChMesh::BaseMesh9::GetOffsetVertex(unsigned long _num)
+{
+	if (offsetVertexList.size() <= _num)return ChVec3_9();
+	return (*offsetVertexList[_num]);
+}
+
+unsigned long ChMesh::BaseMesh9::GetMaterialCount()
+{
+	return material.size();
+}
+
+ChMaterialA_9 ChMesh::BaseMesh9::GetMaterial(unsigned long _num)
+{
+	if (material.size() <= _num)return ChMaterialA_9();
+	return *material[_num];
+}
+
+ChTex::BaseTexture9* ChMesh::BaseMesh9::GetTexture(unsigned long _num)
+{
+	if (texList.size() <= _num)return nullptr;
+	return texList[_num].get();
+}
+
+void ChMesh::BaseMesh9::Release() {
+
+	if (ChPtr::NullCheck(mesh))return;
+
+	if (!material.empty())material.clear();
+
+	if (!texList.empty())texList.clear();
+	if (!normalTex.empty())normalTex.clear();
+	if (!easyFaceList.empty())easyFaceList.clear();
+
+	mesh->Release();
+
+	mesh = nullptr;
 
 }
+
+#endif
+
+#include"ChMeshShared9.h"
 
 #endif
