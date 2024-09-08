45df76a6f35161be3fa51e5b454f666b7fbd82ed
Chronoss0518 <sinyusuke5000@gmail.com>
2024-09-08 14:42:01 +0900

    CRTがないため体裁を整える

--- ChCppLibrary/Direct3D11/ChCB/ChCBCircleCulling/ChCBCircleCulling11.cpp
+++ ChCppLibrary/Direct3D11/ChCB/ChCBCircleCulling/ChCBCircleCulling11.cpp
@@ -8,47 +8,32 @@
 using namespace ChD3D11;
 using namespace CB;
 
-///////////////////////////////////////////////////////////////////////////////////
-//LightHeader Method
-///////////////////////////////////////////////////////////////////////////////////
-
 void CBCircleCulling11::Init(ID3D11Device* _device)
 {
 	Release();
-
 	CBBase11::Init(_device);
-
 	buf.CreateBuffer(_device, CIRCLE_CULLING_DATA);
-
 	SetInitFlg(true);
 }
-
-///////////////////////////////////////////////////////////////////////////////////
 
 void CBCircleCulling11::Release()
 {
 	if (!*this)return;
-
 	buf.Release();
-
 	SetInitFlg(false);
 }
 
 void CBCircleCulling11::SetDrawStartDir(const ChVec2& _drawStartDir)
 {
 	if (!IsInit())return;
-
 	data.drawStartDir = _drawStartDir;
-
 	updateFlg = true;
 }
 
 void CBCircleCulling11::SetCenterPos(const ChVec2& _dirStartPos)
 {
 	if (!IsInit())return;
-
 	data.centerPos = _dirStartPos;
-
 	updateFlg = true;
 }
 
@@ -66,19 +51,15 @@ void CBCircleCulling11::SetDrawValue(const float _drawValue)
 
 void CBCircleCulling11::SetDrawData(ID3D11DeviceContext* _dc)
 {
-
 	if (!*this)return;
 
 	Update(_dc);
-
 	buf.SetToPixelShader(_dc);
-
 }
 
 float CBCircleCulling11::GetDrawValue()
 {
 	if (!IsInit())return 0.0f;
-
 	return data.drawValue;
 }
 
@@ -87,6 +68,5 @@ void CBCircleCulling11::Update(ID3D11DeviceContext* _dc)
 	if (!updateFlg)return;
 
 	buf.UpdateResouce(_dc, &data);
-
 	updateFlg = false;
 }
