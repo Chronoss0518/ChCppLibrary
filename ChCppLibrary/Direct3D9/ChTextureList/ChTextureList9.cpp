4dcd9b50efc7a5b1fc45cc1834e11fe463b4944b
Chronoss0518 <sinyusuke5000@gmail.com>
2024-10-14 17:12:40 +0900

    ・CRTを利用するように修正 ・TargetDirectoryを指定しないように修正 ・wchar_tからTextureを作成できるように修正

--- ChCppLibrary/Direct3D9/ChTextureList/ChTextureList9.cpp
+++ ChCppLibrary/Direct3D9/ChTextureList/ChTextureList9.cpp
@@ -12,6 +12,125 @@ using namespace ChTex;
 //ChTextureList9ÉÅÉ\ÉbÉh
 ///////////////////////////////////////////////////////////////////////////////////////
 
+void ChTex::TextureList9::Release()
+{
+	if (texList.empty())return;
+	texList.clear();
+
+	SetInitFlg(false);
+}
+
+ChTex::Texture9* ChTex::TextureList9::GetTexPtr(const unsigned short dataNum)
+{
+	auto&& tex = texList.find(dataNum);
+	if (tex == texList.end())return nullptr;
+
+	return tex->second.get();
+}
+
+void ChTex::TextureList9::SetTexture(
+	const std::string& _textureName,
+	const unsigned short _dataNum,
+	const unsigned int _gameReSizeWidth,
+	const unsigned int _gameReSizeHeight,
+	const D3DCOLOR _colorKey)
+{
+	if (texList.find(_dataNum) != texList.end())return;
+
+	if (_textureName.length() <= 0)return;
+
+	auto&& tmpTex = ChTex::Texture9::TextureType(_textureName);
+
+	tmpTex->CreateTexture(_textureName, device, _colorKey);
+
+	if (ChPtr::NullCheck(tmpTex->GetTex()))
+	{
+		//ChSystem::ErrerMessage("âÊëúÇ™ì«Ç›çûÇ‹ÇÍÇ‹ÇπÇÒÇ≈ÇµÇΩ", "åxçê");
+
+		tmpTex = nullptr;
+		return;
+	}
+
+	SetTextureBase(tmpTex, _dataNum, _gameReSizeWidth, _gameReSizeHeight, _colorKey);
+}
+
+//TextureÇÃìoò^//
+void ChTex::TextureList9::SetTexture(
+	const std::wstring& _textureName,
+	const unsigned short _dataNum,
+	const unsigned int _gameReSizeWidth,
+	const unsigned int _gameReSizeHeight,
+	const D3DCOLOR _colorKey)
+{
+	if (texList.find(_dataNum) != texList.end())return;
+
+	if (_textureName.length() <= 0)return;
+
+	auto&& tmpTex = ChTex::Texture9::TextureType(_textureName);
+
+	tmpTex->CreateTexture(_textureName, device, _colorKey);
+
+	if (ChPtr::NullCheck(tmpTex->GetTex()))
+	{
+		//ChSystem::ErrerMessage("âÊëúÇ™ì«Ç›çûÇ‹ÇÍÇ‹ÇπÇÒÇ≈ÇµÇΩ", "åxçê");
+
+		tmpTex = nullptr;
+		return;
+	}
+
+	SetTextureBase(tmpTex, _dataNum, _gameReSizeWidth, _gameReSizeHeight, _colorKey);
+}
+
+void ChTex::TextureList9::SetTextureBase(
+	ChPtr::Shared<BaseTexture9> _tex,
+	const unsigned short _dataNum,
+	const unsigned int _gameReSizeWidth,
+	const unsigned int _gameReSizeHeight,
+	const D3DCOLOR _colorKey)
+{
+	_tex->SetSclXSize(1.0f);
+	_tex->SetSclYSize(1.0f);
+
+	{
+		float tmpX, tmpY;
+		tmpX = _gameReSizeWidth > 1.0f ? (float)_gameReSizeWidth / (float)_tex->GetOriginalWidth() : 1.0f;
+		tmpY = _gameReSizeHeight > 1.0f ? (float)_gameReSizeHeight / (float)_tex->GetOriginalHeight() : 1.0f;
+
+		_tex->SetSclXSize(tmpX);
+		_tex->SetSclYSize(tmpY);
+	}
+
+	texList[_dataNum] = _tex;
+}
+
+void ChTex::TextureList9::SetColorTex(
+	const unsigned long& _color,
+	const unsigned short _dataNum,
+	const unsigned int _gameReSizeWidth,
+	const unsigned int _gameReSizeHeight,
+	const unsigned long _type)
+{
+
+	if (texList.find(_dataNum) != texList.end())return;
+
+	auto tmpTex = ChPtr::Make_S<Texture9>();
+
+	tmpTex->CreateColTexture(device, _color);
+
+	tmpTex->SetSclXSize(1.0f);
+	tmpTex->SetSclYSize(1.0f);
+
+	if (_gameReSizeWidth > 0 && _gameReSizeHeight > 0)
+	{
+		tmpTex->SetSclXSize((float)_gameReSizeWidth);
+		tmpTex->SetSclYSize((float)_gameReSizeHeight);
+	}
+
+	texList[_dataNum] = tmpTex;
+
+	return;
+}
+
 void TextureList9::SetBlendColor(
 	const ChVec4& _color,
 	const unsigned short _dataNum)
--- ChCppLibrary/Direct3D9/ChTextureList/ChTextureList9.h
+++ ChCppLibrary/Direct3D9/ChTextureList/ChTextureList9.h
@@ -1,11 +1,7 @@
 #ifndef Ch_D3D9_TexL_h
 #define Ch_D3D9_TexL_h
 
-#ifdef CRT
-
 #include<string>
-
-#endif
 
 namespace ChTex
 {
@@ -26,24 +22,18 @@ namespace ChTex
 	public://Init And Release//
 
 		//もし、XFileフォルダーなどを作っていない場合は第二引数に""を入れる。//
-#ifdef CRT
 		inline void Init(
-			const LPDIRECT3DDEVICE9 _dv,
-			const std::string _fileInDirectoryPath)
+			const LPDIRECT3DDEVICE9 _dv)
 		{
 			device = _dv;
-
-			directoryPath = _fileInDirectoryPath;
 
 			SetInitFlg(true);
 		}
 
-#endif
 		void Release();
 
 	public://Set Function//
 
-#ifdef CRT
 		//Textureの登録//
 		void SetTexture(
 			const std::string& _textureName,
@@ -52,7 +42,26 @@ namespace ChTex
 			const unsigned int _gameReSizeHeight,
 			const D3DCOLOR _colorKey = D3DCOLOR_ARGB(0, 0, 0, 0)
 		);
-#endif
+
+		//Textureの登録//
+		void SetTexture(
+			const std::wstring& _textureName,
+			const unsigned short _dataNum,
+			const unsigned int _gameReSizeWidth,
+			const unsigned int _gameReSizeHeight,
+			const D3DCOLOR _colorKey = D3DCOLOR_ARGB(0, 0, 0, 0)
+		);
+
+	protected:
+
+		void SetTextureBase(
+			ChPtr::Shared<BaseTexture9> _tex,
+			const unsigned short _dataNum,
+			const unsigned int _gameReSizeWidth,
+			const unsigned int _gameReSizeHeight,
+			const D3DCOLOR _colorKey = D3DCOLOR_ARGB(0, 0, 0, 0));
+
+	public:
 
 		//合成可色、基本色の設定//
 		void SetBlendColor(
@@ -75,7 +84,6 @@ namespace ChTex
 
 	public://Get Functions//
 
-#ifdef CRT
 		//外部利用するためのテクスチャを取得する//
 		inline ChPtr::Shared<Texture9> GetTex(const unsigned short dataNum)
 		{
@@ -84,7 +92,6 @@ namespace ChTex
 			return texList[dataNum];
 
 		}
-#endif
 
 	public://Get Functions//
 
@@ -93,137 +100,12 @@ namespace ChTex
 	private:
 
 		LPDIRECT3DDEVICE9 device = nullptr;
-#ifdef CRT
-		std::string directoryPath;
-
+		
 		std::map<unsigned short, ChPtr::Shared<Texture9>>texList;
-#endif
 
 	}ChTexList9;
 
 }
-
-#ifdef CRT
-
-void ChTex::TextureList9::Release()
-{
-	if (texList.empty())return;
-	texList.clear();
-
-	SetInitFlg(false);
-}
-
-ChTex::Texture9* ChTex::TextureList9::GetTexPtr(const unsigned short dataNum)
-{
-	auto&& tex = texList.find(dataNum);
-	if (tex == texList.end())return nullptr;
-
-	return tex->second.get();
-}
-
-void ChTex::TextureList9::SetTexture(
-	const std::string& _textureName,
-	const unsigned short _dataNum,
-	const unsigned int _gameReSizeWidth,
-	const unsigned int _gameReSizeHeight,
-	const D3DCOLOR ColorKey)
-{
-	if (texList.find(_dataNum) != texList.end())return;
-
-	if (_textureName.length() <= 0)return;
-
-	std::string tmpPath = "./";
-
-	if (directoryPath.length() > 0)
-	{
-		tmpPath = directoryPath;
-		tmpPath += "/";
-	}
-
-	std::string tmpString = _textureName;
-
-	tmpString = tmpPath + tmpString;
-
-	auto tmpTex = ChTex::Texture9::TextureType(tmpString);
-
-	tmpTex->CreateTexture(tmpString, device, ColorKey);
-
-	if (ChPtr::NullCheck(tmpTex->GetTex()))
-	{
-		//ChSystem::ErrerMessage("画像が読み込まれませんでした", "警告");
-
-		tmpTex = nullptr;
-		return;
-	}
-
-	tmpTex->SetSclXSize(1.0f);
-	tmpTex->SetSclYSize(1.0f);
-
-	{
-		float tmpX, tmpY;
-		tmpX = _gameReSizeWidth > 1.0f ? (float)_gameReSizeWidth / (float)tmpTex->GetOriginalWidth() : 1.0f;
-		tmpY = _gameReSizeHeight > 1.0f ? (float)_gameReSizeHeight / (float)tmpTex->GetOriginalHeight() : 1.0f;
-
-		tmpTex->SetSclXSize(tmpX);
-		tmpTex->SetSclYSize(tmpY);
-	}
-
-	texList[_dataNum] = tmpTex;
-
-}
-
-void ChTex::TextureList9::SetColorTex(
-	const unsigned long& _color,
-	const unsigned short _dataNum,
-	const unsigned int _gameReSizeWidth,
-	const unsigned int _gameReSizeHeight,
-	const unsigned long _type)
-{
-
-	if (texList.find(_dataNum) != texList.end())return;
-
-	auto tmpTex = ChPtr::Make_S<Texture9>();
-
-	tmpTex->CreateColTexture(device, _color);
-
-	tmpTex->SetSclXSize(1.0f);
-	tmpTex->SetSclYSize(1.0f);
-
-	if (_gameReSizeWidth > 0 && _gameReSizeHeight > 0)
-	{
-		tmpTex->SetSclXSize((float)_gameReSizeWidth);
-		tmpTex->SetSclYSize((float)_gameReSizeHeight);
-	}
-
-	texList[_dataNum] = tmpTex;
-
-	return;
-}
-
-void BaseMesh9::SetOffsetVertex()
-{
-	if (ChPtr::NullCheck(mesh))return;
-
-	MeshVertex9* tmpVer = nullptr;
-	mesh->LockVertexBuffer(NULL, (LPVOID*)&tmpVer);
-
-	if (ChPtr::NullCheck(tmpVer))return;
-
-	for (unsigned long ver = 0; ver < mesh->GetNumVertices(); ver++)
-	{
-		auto tmpPos = ChPtr::Make_S<ChVec3_9>();
-
-		*tmpPos = (tmpVer + ver)->pos;
-
-		offsetVertexList.push_back(tmpPos);
-	}
-
-
-	mesh->UnlockVertexBuffer();
-	return;
-}
-
-#endif
 
 #endif
 //CopyRight Chronoss0518 2018/0
