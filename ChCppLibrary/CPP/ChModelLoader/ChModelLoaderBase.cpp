d8ded155f083a5430da97ace900c3baa0cc98c54
Chronoss0518 <sinyusuke5000@gmail.com>
2024-11-17 15:51:31 +0900

    関数名を修正

--- ChCppLibrary/CPP/ChModelLoader/ChModelLoaderBase.cpp
+++ ChCppLibrary/CPP/ChModelLoader/ChModelLoaderBase.cpp
@@ -7,12 +7,12 @@ template<> std::basic_string<char> ChCpp::GetConvertText(const std::string& _str
 
 template<> std::basic_string<wchar_t> ChCpp::GetConvertText(const std::string& _str)
 {
-	return ChStr::GetShiftJisFromUTF16(_str);
+	return ChStr::GetUTF16FromShiftJis(_str);
 }
 
 template<> std::basic_string<char> ChCpp::GetConvertText(const std::wstring& _str)
 {
-	return ChStr::GetUTF16FromShiftJis(_str);
+	return ChStr::GetShiftJisFromUTF16(_str);
 }
 
 template<> std::basic_string<wchar_t> ChCpp::GetConvertText(const std::wstring& _str)
